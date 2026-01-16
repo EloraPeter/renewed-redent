36536b061aed4c362765f60dc30b84a5ca0530c5
elorapeter <florenceonyi09@gmail.com>
2026-01-16 07:53:40 +0000

    Jan 16, 2026, 8:53 AM

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,6 @@
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
 
-export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
-  const session = await getServerSession();
-
-  if (!session?.user?.role) {
-    redirect("/role-select");
-  }
-
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return children;
 }
